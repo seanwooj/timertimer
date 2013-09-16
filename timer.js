2b058015a883e79819341edce901a814b5f943c1
Ned Ruggeri <ruggeri@self-loop.com>
2013-09-15 23:35:37 -0700

    hack hack hack

--- timer.js
+++ timer.js
@@ -1,16 +1,36 @@
 (function () {
   var T3 = window.T3 = {};
 
-  var Controller = Timer.T3 = function (timerContainer) {
-    this.$timerContainer = $(timerContainer);
+  var Controller = T3.Controller = function ($timerContainer, $buttons) {
+    this.$timerContainer = $timerContainer;
+    this.$buttons = $buttons;
+
+    this.$buttons.on("click", this.handleButtonClick.bind(this));
 
     this.secondsLeft = 0;
     this.lastTick = undefined;
     this.timerId = undefined;
   };
 
+  Controller.prototype.handleButtonClick = function (button) {
+    var $button = $(button.currentTarget);
+    var seconds = $button.attr('data-time');
+    console.log($button);
+    this.startTimer(seconds);
+
+    // TODO: Cookie loader...
+
+    /*
+    var type = $(this).html();
+    $.cookie("timerCreated", $.now(), { expires: 1 } );
+    $.cookie("timerLength", seconds, { expires: 1 } );
+    this.saveCookieHistory(type, $.now());
+    this.loadCookieHistory();
+    */
+  }
+
   Controller.prototype.startTimer = function (seconds) {
-    this.secondsLeft = secondsLeft;
+    this.secondsLeft = seconds;
     this.lastTick = new Date().getTime();
 
     if (this.timerId !== undefined) {
@@ -20,7 +40,7 @@
     this.timerId = setInterval(this.tick.bind(this), 1000);
   };
 
-  function formatTimeString = function (seconds) {
+  function formatTimeString (seconds) {
     var minutes = Math.floor(seconds / 60);
     var seconds = seconds % 60;
 
@@ -37,10 +57,10 @@
 
   Controller.prototype.tick = function () {
     var now = new Date().getTime();
-    var millisElapsed = this.lastTick - now;
+    var millisElapsed = now - this.lastTick;
 
     this.lastTick = now;
-    this.secondsLeft -= millisElapsed / 1000;
+    this.secondsLeft -= Math.floor(millisElapsed / 1000);
 
     if (this.secondsLeft <= 0 ) {
       clearInterval(this.timerId);
@@ -53,7 +73,7 @@
     $('title').html(timeString);
   };
 
-  this.ding = function () {
+  Controller.prototype.ding = function () {
     this.$timerContainer.html("00:00");
     $('title').html('Ding!');
 
@@ -72,63 +92,11 @@
     $('body').append($ring);
   };
 
-  var CookieLoader = T3.CookieLoader = function () {
-  };
-
-  CookieLoader.prototype.loadCookieTimerData = function () {
-    if ( $.cookie("timerCreated") ) {
-      var timeSince = Math.floor(($.now() - $.cookie('timerCreated'))/1000);
-      var remainingTime = $.cookie("timerLength") - timeSince;
-      
-      if (remainingTime > 0) {
-        that.startTimer(remainingTime);
-      }
-      return true;
-    }
-  };
-
-  CookieLoader.prototype.load = function () {
-    $('.stats-table').empty();
-    if ( $.cookie("history") ) {
-      $.each($.cookie("history"), function(key, value) {
-        timeAgo = Math.floor(($.now() - value.startedAt)/1000/60);
-        if ( timeAgo >= 60 ) {
-          timeAgo = Math.floor( timeAgo / 60 );
-          $('.stats-table').prepend('<div class="stats-row"><span class="stat-key">' + value.type +'</span><span class="stat-value">about ' + timeAgo + ' hours ago</span></div>');
-        } else {
-          $('.stats-table').prepend('<div class="stats-row"><span class="stat-key">' + value.type +'</span><span class="stat-value">' + timeAgo + ' minutes ago</span></div>');
-        }
-      });
-    }
-  };
-
-  CookieLoader.prototype.saveCookieHistory = function (type, startedAt) {
-    if ( $.cookie("history") ) {
-      var oldData = $.cookie("history");
-      if ( oldData.length > 5 ) {
-        oldData.shift();
-      }
-      oldData.push( { "type" : type , "startedAt": startedAt } );
-      $.cookie("history", oldData);
-    } else {
-      $.cookie("history", [ { "type" : type , "startedAt": startedAt } ]);
-    }
-    // console.log($.cookie("history"));
-  };
-
-  this.start = function(timerLinkClass) {
+  this.start = function () {
     $.cookie.json = true;
     that.loadCookieTimerData();
     that.loadCookieHistory();
-    $(timerLinkClass).click(function() {
-      time = $(this).attr('time-data');
-      type = $(this).html();
-      that.startTimer(time);
-      $.cookie("timerCreated", $.now(), { expires: 1} );
-      $.cookie("timerLength", time, { expires: 1} );
-      that.saveCookieHistory(type, $.now());
-      that.loadCookieHistory();
-    });
+
     $('.custom-timer').submit(function() {
       time = $('input.minutes-input').val();
       timerName = $('input.name-input').val();
@@ -145,5 +113,49 @@
 
       modal.close();
     });
+  };
+
+  // Cookie bullshit
+  var CookieLoader = T3.CookieLoader = {
+    loadCookieTimerData: function () {
+      if ( $.cookie("timerCreated") ) {
+        var timeSince = Math.floor(($.now() - $.cookie('timerCreated'))/1000);
+        var remainingTime = $.cookie("timerLength") - timeSince;
+
+        if (remainingTime > 0) {
+          that.startTimer(remainingTime);
+        }
+        return true;
+      }
+    },
+
+    load: function () {
+      $('.stats-table').empty();
+      if ( $.cookie("history") ) {
+        $.each($.cookie("history"), function(key, value) {
+          timeAgo = Math.floor(($.now() - value.startedAt)/1000/60);
+          if ( timeAgo >= 60 ) {
+            timeAgo = Math.floor( timeAgo / 60 );
+            $('.stats-table').prepend('<div class="stats-row"><span class="stat-key">' + value.type +'</span><span class="stat-value">about ' + timeAgo + ' hours ago</span></div>');
+          } else {
+            $('.stats-table').prepend('<div class="stats-row"><span class="stat-key">' + value.type +'</span><span class="stat-value">' + timeAgo + ' minutes ago</span></div>');
+          }
+        });
+      }
+    },
+
+    saveCookieHistory: function (type, startedAt) {
+      if ( $.cookie("history") ) {
+        var oldData = $.cookie("history");
+        if ( oldData.length > 5 ) {
+          oldData.shift();
+        }
+        oldData.push( { "type" : type , "startedAt": startedAt } );
+        $.cookie("history", oldData);
+      } else {
+        $.cookie("history", [ { "type" : type , "startedAt": startedAt } ]);
+      }
+      // console.log($.cookie("history"));
+    }
   };
 })();
