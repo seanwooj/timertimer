e0ad0d51bc0c622ad2f8bbd6181bd91bf8398f37
seanwooj <seanwoojinkim@gmail.com>
2013-10-01 13:46:39 -0700

    added querystring reader

--- timer.js
+++ timer.js
@@ -178,4 +178,13 @@
     oldData.push({ "type" : type , "startedAt": startedAt });
     $.cookie("history", oldData);
   };
+
+  var QueryStringReader = T3.QueryStringReader = {};
+
+  QueryStringReader.getParameterByName = function (name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    var results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
 })();
